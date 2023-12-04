b1d671e5952ca3ab000eaf3158e0c3572dad801f
Harman Patrik (SZF_2023_2025) <harman.patrik@diak.szamalk-szalezi.hu>
2023-12-04 12:07:43 +0100

    oraimunka

---
+++ main.py
@@ -0,0 +1,36 @@
+import Szamitogep
+import elsof
+import masodikF
+import harmadikF
+import negyedikF
+import hetedikF
+
+"""
+szg1 = Szamitogep.Szamitogep(0,False)
+szg2 = Szamitogep.Szamitogep(0,False)
+
+szg1.kapcsol()
+if szg1.programMasol(800):
+    print("A másolás sikeres volt")
+else:
+    print("A másolás sikertelen volt")
+
+if szg1.programMasol(400):
+    print("A másolás sikeres volt")
+else:
+    print("A másolás sikertelen volt")
+
+
+if szg2.programMasol(1):
+    print("A másolás sikeres volt")
+else:
+    print("A másolás sikertelen volt")
+
+print(szg1)
+print(szg2)
+"""
+#elsof.elso()
+#masodikF.masodikF
+#harmadikF.harmadik()
+#negyedikF.negyedik()
+hetedikF.hetedik()
